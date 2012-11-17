60f5f6a9e2a4b8f2ef4fffae046729774255fa34
Ross D <rgd@crystal.ece.cmu.edu>
2012-11-17 02:28:55 -0500

    added more to shortstack

--- RAYTRACER/shortstack/shortstack.sv
+++ RAYTRACER/shortstack/shortstack.sv
@@ -156,10 +156,10 @@ end
   } stack_write_fifo_in[2], stack_write_fifo_out[2];
 
   // fifo to accumulate Definite misses and definite hits
-  logic stack_write_fifo_full[2];
-  logic stack_write_fifo_empty[2];
-  logic stack_write_fifo_re[2];
-  logic stack_write_fifo_we[2];
+  logic [1:0] stack_write_fifo_full;
+  logic [1:0] stack_write_fifo_empty;
+  logic [1:0] stack_write_fifo_re;
+  logic [1:0] stack_write_fifo_we;
   always_comb begin
     stack_write_fifo_in.rayID[0] = trav0_to_ss_data.ray_info.rayID;
     stack_write_fifo_in.ss_wptr[0] = trav0_to_ss_data.ray_info.ss_wptr;
@@ -193,42 +193,126 @@ endgenerate
 
 //------------------------------------------------------------------------
 // Stack instantiations
+
+  logic [1:0] stack_r_rrptr, stack_r_rrptr_n;
+  assign r_rrptr_n = (|trans_choice) ? (stack_r_rrptr == 2'h2 ? 2''h0 : stack_r_rrptr + 1'b1) : stacK_r_rrptr ;
+  ff_ar #(2,2'b0) stack_r_rrptr_buf(.d(stack_r_rrptr_n), .q(stack_r_rrptr), .clk, .rst);
+  
+  logic [2:0] stack_rfifo_valid, stack_rfifo_choice;
+  assign stack_rfifo_valid = ~stack_read_fifo_empty & {3{stack_VSpipe_stall_us}};
+  always_comb begin
+    stack_rfifo_choice = 'h0;
+    stack_rfifo_choice[stack_r_rrptr] = stack_rfifo_valid[stack_r_rrptr];
+    stack_rfifo_choice[stack_r_rrptr+2'b01] = stack_rfifo_valid[stack_r_rrptr+2'b01] & ~stack_rfifo_choice[stack_r_rrptr] ;
+    stack_rfifo_choice[stack_r_rrptr+2'b10] = stack_rfifo_valid[stack_r_rrptr+2'b10] & ~stack_rfifo_choice[stack_r_rrptr] & ~stack_rfifo_chocie[stack_r_rrptr+2'b01];
+  end
+  
+  logic [3:0] stack_read_valid;
+  logic [1:0] stack_cur_rptr;
+  rayID_t stack_cur_raddr;
+  always_comb begin
+    stack_cur_rptr = 'h0;
+    stack_read_valid = 'h0;
+    case(stack_rfifo_choice)
+     3'b100: begin
+        stack_cur_rptr = stack_read_fifo_out[2].ray_info.ss_wptr;
+        stack_cur_raddr = stack_read_fifo_out[2].ray_info.rayID;
+     3'b010: begin
+        stack_cur_rptr = stack_read_fifo_out[1].ray_info.ss_wptr;
+        stack_cur_raddr = stack_read_fifo_out[1].ray_info.rayID;
+     3'b001: begin
+        stack_cur_rptr = stack_read_fifo_out[0].ray_info.ss_wptr;
+        stack_cur_raddr = stack_read_fifo_out[0].ray_info.rayID;
+      end
+    endcase
+    stack_read_valid[stack_cur_rptr] = 1'b1 ;
+  end
+  logic stack_is_reading;
+  assign stack_is_reading = |stack_read_valid;
+
+  logic stack_w_rrptr, stack_w_rrptr_n;
+  assign w_rrptr_n = ( ) ?  ~stack_w_rrptr : stacK_w_rrptr ;
+  ff_ar #(1,1'b0) stack_w_rrptr_buf(.d(stack_w_rrptr_n), .q(stack_w_rrptr), .clk, .rst);
+  
+  logic [1:0] stack_wfifo_valid; // if not stalling and there is something in the write fifo
+  logic [1:0] stack_wfifo_choice; // Both bits CAN be set here (resolves read/write conflict)
+  logic stack_same_w0w1_dest;
+  logic stack_same_rw0_dest;
+  logic stack_same_rw1_dest;
+  assign stack_same_w0w1_dest = (stack_write_fifo_out[0].ss_wptr == stack_write_fifo_out[1].ss_wptr);
+  assign stack_same_rw0_dest = (stack_cur_rptr == stack_write_fifo_out[0].ss_wptr);
+  assign stack_same_rw1_dest = (stack_cur_rptr == stack_write_fifo_out[1].ss_wptr);
+
+  assign stack_wfifo_valid = ~stack_write_fifo_empty;
+  
+  
+  always_comb begin
+    if(stack_w_rrptr) begin
+      stack_wfifo_choice[1] = stack_wfifo_valid[1];
+      stack_wfifo_choice[0] = stack_wfifo_valid[0] & ~(stack_same_w0w1_dest & stack_same_rw0_dest & 
+                                                       stack_is_reading & stack_wfifo_valid[1]) ;
+    else begin
+      stack_wfifo_choice[0] = stack_wfifo_valid[0];
+      stack_wfifo_choice[1] = stack_wfifo_valid[1] & ~(stack_same_w0w1_dest & stack_same_rw0_dest & 
+                                                       stack_is_reading & stack_wfifo_valid[0]) ;
+    end
+  end
+  // port A = 0, port B = 1
+
+  logic stack_w0_port;
+  logic stack_w1_port;
+  logic [1:0][1:0] stack_wptr;
+  rayID_t 
+  always_comb begin
+    if(stack_w_rrptr) begin
+      stack_w1_port = (stack_is_reading & stack_same_rw1_dest) ? 1'b1 : 1'b0 ;
+      stack_w0_port = (stack_is_reading & stack_same_rw0_dest)|(stack_wfifo_choice[1] & stack_same_w0w1_dest) ? 1'b1 : 1'b0 ;
+   end
+    else begin
+      stack_w0_port = (stack_is_reading & stack_same_rw0_dest) ? 1'b1 : 1'b0 ;
+      stack_w1_port = (stack_is_reading & stack_same_rw1_dest)|(stack_wfifo_choice[0] & stack_same_w0w1_dest) ? 1'b1 : 1'b0 ;
+    end
+  end
+  
+  
   rayID_t addrA_stack[4], addrB_stack[4];
   ss_elem_t wrdataA_stack[4];
   ss_elem_t wrdataB_stack[4];
   logic [3:0] wrenA_stack, wrenB_stack;
   ss_elem_t rddataA_stack[4];
   ss_elem_t rddataB_stack[4];
-
-  logic [1:0] stack_rr_ptr, stack_rr_ptr_n;
-  assign rr_ptr_n = ( ) ? (stack_rr_ptr == 2'h2 ? 2''h0 : stack_rr_ptr + 1'b1) : stacK_rr_ptr ;
-  ff_ar #(1,1'b0) stack_rr_ptr_buf(.d(stack_rr_ptr_n), .q(stack_rr_ptr), .clk, .rst);
-  
-  logic [2:0] trans_valid, trans_choice;
-  assign trans_valid = ~stack_write_fifo_empty & {3{stack_VSpipe_stall_us}};
+ 
   always_comb begin
-    trans_choice = 'h0;
-    trans_choice[stack_rr_ptr] = trans_valid[stack_rr_ptr];
-    trans_choice[stack_rr_ptr+2'b01] = trans_valid[stack_rr_ptr+2'b01] & ~trans_choice[stack_rr_ptr] ;
-    trans_choice[stack_rr_ptr+2'b10] = trans_valid[stack_rr_ptr+2'b10] & ~trans_choice[stack_rr_ptr] & ~trans_chocie[stack_rr_ptr+2'b01];
+    for(int i=0; i<3; i++) begin
+      wrenA_stack[i] = (stack_wfifo_choice[1] & stack_wptr[1] == i & ~stack_w1_port) | (stack_wfifo_choic[0] & stack_wptr[0] == i & ~stack_w0_port) ;
+      wrenB_stack[i] = (stack_wfifo_choice[1] & stack_wptr[1] == i & stack_w1_port) | (stack_wfifo_choic[0] & stack_wptr[0] == i & stack_w0_port) ;
+      if(stack_w_rrptr) begin
+        addrA_stack[i] = (stack_read_valid[i] & cur_rptr==i) ? stack_cur_raddr : 
+                         ( (stack_wfifo_choice[1] & stack_wptr[1]==i & ~stack_w1_port) ? stack_write_fifo_out[1].rayID : 
+                           (stack_wfifo_choice[0] & stack_wptr[0]==i & ~stack_w0_port) ? stack_write_fifo_out[0].rayID) : `DC ;
+        wrdataA_stack[i] = (stack_wfifo_choice[1] & stack_wptr[1]==i & ~stack_w1_port) ? stack_write_fifo_out[1].elem :
+                           (stack_wfifo_choice[0] & stack_wptr[0]==i & ~stack_w0_port) ? stack_write_fifo_out[0].elem : `DC;
+        addrB_stack[i] = (stack_wfifo_choice[1] & stack_wptr[1]==i & stack_w1_port) ? stack_write_fifo_out[1].rayID :
+                         (stack_wfifo_choice[0] & stack_wptr[0]==i & stack_w0_port) ? stack_write_fifo_out[0].rayID : `DC;
+        wrdataB_stack[i] = (stack_wfifo_choice[1] & stack_wptr[1]==i & stack_w1_port) ? stack_write_fifo_out[1].elem :
+                           (stack_wfifo_choice[0] & stack_wptr[0]==i & stack_w0_port) ? stack_write_fifo_out[0].elem : `DC;
+      end
+      else begin
+        addrA_stack[i] = (stack_read_valid[i] & cur_rptr==i) ? stack_cur_raddr : 
+                         ( (stack_wfifo_choice[0] & stack_wptr[0]==i & ~stack_w0_port) ? stack_write_fifo_out[0].rayID : 
+                           (stack_wfifo_choice[1] & stack_wptr[1]==i & ~stack_w1_port) ? stack_write_fifo_out[1].rayID) : `DC ;
+        wrdataA_stack[i] = (stack_wfifo_choice[0] & stack_wptr[0]==i & ~stack_w0_port) ? stack_write_fifo_out[0].elem :
+                           (stack_wfifo_choice[1] & stack_wptr[1]==i & ~stack_w1_port) ? stack_write_fifo_out[1].elem : `DC;
+        addrB_stack[i] = (stack_wfifo_choice[0] & stack_wptr[0]==i & stack_w0_port) ? stack_write_fifo_out[0].rayID :
+                         (stack_wfifo_choice[1] & stack_wptr[1]==i & stack_w1_port) ? stack_write_fifo_out[1].rayID : `DC;
+        wrdataB_stack[i] = (stack_wfifo_choice[0] & stack_wptr[0]==i & stack_w0_port) ? stack_write_fifo_out[0].elem :
+                           (stack_wfifo_choice[1] & stack_wptr[1]==i & stack_w1_port) ? stack_write_fifo_out[1].elem : `DC;
+    
+      end
+    end
   end
   
-  assign stack_read_fifo_re = trans_choice;
-  
-  logic [3:0] stack_read_en;
-  always_comb begin
-    stack_read_en = 4'h0;
-    stack_read_en[stack_read_fifo_out[trans_choice].ray_info.ss_wptr]
-  end
 
-  // reading always has addrA priority
-  
-  assign addrA_stack = ;
-  assign wrdataA_stack = ;
-  assign wrenA_stack = ;
-  assign addrB_stack = ;
-  assign wrdataB_stack = ;
-  assign wrenB_stack = ;
 
 genvar s;
 generate
@@ -243,7 +327,7 @@ generate
     .wren_a(wrenA_stack[i]),
     .wren_b(wrenB_stack[i]),
     .q_a(rddataA_stack[i]),
-    .q_b(rddataB_stack[i]));
+    .q_b());
   end
 
 endgenerate
@@ -266,10 +350,10 @@ endgenerate
       3'b100 : stack_VS_pipe_in = read_fifo_out[2];
       3'b010 : stack_VS_pipe_in = read_fifo_out[1];
       3'b001 : stack_VS_pipe_in = read_fifo_out[0];
-      3'b000 : stack_VS_pipe_in = 'hX;
+      3'b000 : stack_VS_pipe_in = `DC;
     endcase
   end
-  assign stack_VSpipe_valid_us = ;
+  assign stack_VSpipe_valid_us = stack_is_reading;
   
 
   pipe_valid_stall #(.WIDTH($bits(stack_VSpipe_in)), .DEPTH(4)) stack_VSpipe_inst(
@@ -291,7 +375,6 @@ endgenerate
     nodeID_t nodeID;
   } stack_fifo_in, stack_fifo_out;
 
-  // fifo to accumulate Definite misses and definite hits
   logic stack_fifo_full;
   logic stack_fifo_empty;
   logic stack_fifo_re;
@@ -320,26 +403,114 @@ endgenerate
 
 
 
+//------------------------------------------------------------------------------------------------------
+// Restart Patha
+//------------------------------------------------------------------------------------------------------
 
 
+// Short Stack Read fifos
+  // 0 == trav0
+  // 1 == trav1
+  // 2 == list
+
+  struct packed {
+    ray_info_t ray_info; // This has wptr representing the readpointer (ss_wptr - 1)
+    float_t t_min;
+  } rest_read_fifo_in[3], rest_read_fifo_out[3];
+
+  logic [2:0] rest_read_fifo_full;
+  logic [2:0] rest_read_fifo_empty;
+  logic [2:0] rest_read_fifo_re;
+  logic [2:0] rest_read_fifo_we;
+  always_comb begin
+    rest_read_fifo_in.ray_info[0] = trav0_to_ss_data.ray_info;
+    rest_read_fifo_in.t_min[0] =  trav0_to_ss_data.t_max;
+    rest_read_fifo_in.ray_info[1] = trav1_to_ss_data.ray_info;
+    rest_read_fifo_in.t_min[1] =  trav1_to_ss_data.t_max;
+    rest_read_fifo_in.ray_info[2] = list_to_ss_data.ray_info;
+    rest_read_fifo_in.t_min[2] = list_to_ss_data.t_max;
+  end
+
+  assign rest_read_fifo_we = ;
 
 
+genvar i;
+generate begin
+  for(i=0; i<3; i+=1) begin
+  fifo #(.DEPTH(3), .WIDTH($bits(rest_read_fifo_in)) ) rest_read_fifo_inst(
+    .clk, .rst,
+    .data_in(rest_read_fifo_in[i]),
+    .data_out(rest_read_fifo_out[i]),
+    .full(rest_read_fifo_full[i]),
+    .empty(rest_read_fifo_empty[i]),
+    .re(rest_read_fifo_re[i]),
+    .we(rest_read_fifo_we[i]),
+    .num_left_in_fifo(),
+    .exists_in_fifo());
+  end
+end
+
+//------------------------------------------------------------------------
+// Short Stack write fifos
+  // 0 == trav0
+  // 1 == trav1
+  // 2 == sint
+
+  struct packed {
+    rayID_t rayID;
+    ss_elem_t elem;
+  } rest_write_fifo_in[3], rest_write_fifo_out[3];
+
+  // fifo to accumulate Definite misses and definite hits
+  logic [2:0] rest_write_fifo_full;
+  logic [2:0] rest_write_fifo_empty;
+  logic [2:0] rest_write_fifo_re;
+  logic [2:0] rest_write_fifo_we;
+  always_comb begin
+    rest_write_fifo_in.rayID[0] = trav0_to_ss_data.ray_info.rayID;
+    rest_write_fifo_in.elem.nodeID[0] =  trav0_to_ss_data.rest_node_ID;
+    rest_write_fifo_in.elem.t_max[0] =  trav0_to_ss_data.t_max;
+    rest_write_fifo_in.rayID[1] = trav1_to_ss_data.ray_info.rayID;
+    rest_write_fifo_in.elem.nodeID[1] =  trav1_to_ss_data.rest_node_ID;
+    rest_write_fifo_in.elem.t_max[1] =  trav1_to_ss_data.t_max;
+    rest_write_fifo_in.rayID[2] = sint_to_ss_data.ray_info.rayID;
+    rest_write_fifo_in.elem.nodeID[2] =  sint_to_ss_data.rest_node_ID;
+    rest_write_fifo_in.elem.t_max[2] =  sint_to_ss_data.t_max;
+  end
+
+  assign rest_write_fifo_re = ;
+  assign rest_write_fifo_we = ;
 
 
-
-
-
-
-
-
-
-
+genvar i;
+generate begin
+  for(i=0; i<2; i+=1) begin
+  fifo #(.DEPTH(3), .WIDTH($bits(rest_write_fifo_in)) ) rest_write_fifo_inst(
+    .clk, .rst,
+    .data_in(rest_write_fifo_in[i]),
+    .data_out(rest_write_fifo_out[i]),
+    .full(rest_write_fifo_full[i]),
+    .empty(rest_write_fifo_empty[i]),
+    .re(rest_write_fifo_re[i]),
+    .we(rest_write_fifo_we[i]),
+    .num_in_fifo() );
+  end
+endgenerate
 
 
 //------------------------------------------------------------------------
-// Restart Path
 
-//------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
 
 
 
