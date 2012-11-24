c3a5e8dcc45d3bad2dabf6b693fafb57b2fef6a8
Paul Kennedy <pmkenned@columbus.ece.cmu.edu>
2012-11-24 15:11:35 -0500

    Added shader cache parameters

--- TBs/t15_tb.sv
+++ TBs/t15_tb.sv
@@ -50,6 +50,38 @@ module t15_tb;
  
     logic clk;
 
+    //////////// BIG ASS ASSERTION ////////////
+
+	logic big_ass_assertion;
+assign big_ass_assertion = (t15.rp.prg_to_shader_valid & ~t15.rp.prg_to_shader_stall) |
+                           (t15.rp.shader_to_sint_valid & ~t15.rp.shader_to_sint_stall) |
+                           (t15.rp.sint_to_shader_valid & ~t15.rp.sint_to_shader_stall) |
+                           (t15.rp.sint_to_ss_valid & ~t15.rp.sint_to_ss_stall) |
+                           (t15.rp.sint_to_tarb_valid & ~t15.rp.sint_to_tarb_stall) |
+                           (t15.rp.tarb_to_tcache0_valid & ~t15.rp.tarb_to_tcache0_stall) |
+                           (t15.rp.tcache_to_trav0_valid & ~t15.rp.tcache_to_trav0_stall) |
+                           (t15.rp.trav0_to_rs_valid & ~t15.rp.trav0_to_rs_stall) |
+                           (t15.rp.rs_to_trav0_valid & ~t15.rp.rs_to_trav0_stall) |
+                           (t15.rp.trav0_to_tarb_valid & ~t15.rp.trav0_to_tarb_stall) |
+                           (t15.rp.trav0_to_ss_valid & ~t15.rp.trav0_to_ss_stall) |
+                           (t15.rp.trav0_to_list_valid & ~t15.rp.trav0_to_list_stall) |
+                           (t15.rp.trav0_to_larb_valid & ~t15.rp.trav0_to_larb_stall) |
+                           (t15.rp.larb_to_lcache_valid & ~t15.rp.larb_to_lcache_stall) |
+                           (t15.rp.lcache_to_icache_valid & ~t15.rp.lcache_to_icache_stall) |
+                           (t15.rp.icache_to_rs_valid & ~t15.rp.icache_to_rs_stall) |
+                           (t15.rp.rs_to_int_valid & ~t15.rp.rs_to_int_stall) |
+                           (t15.rp.int_to_larb_valid & ~t15.rp.int_to_larb_stall) |
+                           (t15.rp.int_to_list_valid & ~t15.rp.int_to_list_stall) |
+                           (t15.rp.list_to_rs_valid & ~t15.rp.list_to_rs_stall) |
+                           (t15.rp.list_to_ss_valid & ~t15.rp.list_to_ss_stall) |
+                           (t15.rp.ss_to_tarb_valid0 & ~t15.rp.ss_to_tarb_stall0) |
+                           (t15.rp.ss_to_tarb_valid1 & ~t15.rp.ss_to_tarb_stall1) |
+                           (t15.rp.ss_to_shader_valid & ~t15.rp.ss_to_shader_stall) |
+                           (t15.rp.rs_to_pcalc_valid & ~t15.rp.rs_to_pcalc_stall) |
+                           (t15.rp.pcalc_to_shader_valid & ~t15.rp.pcalc_to_shader_stall);
+
+    //////////// END OF BIG ASS ASSERTION ////////////
+
     //////////// pixel ID checker code ////////////
 	bit [`MAX_PIXEL_IDS][$bits(pixelID_t)-1:0] pixelIDs_us ;
 	bit [`MAX_PIXEL_IDS][$bits(pixelID_t)-1:0] pixelIDs_ds ;
