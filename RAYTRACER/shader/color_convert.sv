49711ef2f7ccd000e94381a1222eca8ed4ed75ee
Paul Kennedy <pmkenned@columbus.ece.cmu.edu>
2012-11-25 13:38:33 -0500

    Created color_convert. Not at all done. Added bunny scene file

---
+++ RAYTRACER/shader/color_convert.sv
@@ -0,0 +1,20 @@
+// OBVIOUSLY THIS FILE IS NOT NEAR COMPLETE
+module color_convert(
+	input logic clk, rst,
+	input vector_t color_in,
+	input logic v0, v1, v2,
+	output logic [7:0] color_out
+);
+
+	fp_mult fpm();
+
+	fp_to_int fpti();
+
+	logic less_than_0, greater_than_255;
+
+	assign less_than_0 = int_value < 0;
+	assign greater_than_255 = int_value > 255;
+
+	assign color_out = (less_than_0) 
+
+endmodule
